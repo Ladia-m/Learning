c6ae716a03631357f87bc469412fddf2086a2209
Ladia-M <ladislav.miko.study@email.cz>
2021-04-02 23:04:42 +0200

    Pong: using super() with paddle and first move

--- Python/Pong/components.py
+++ Python/Pong/components.py
@@ -8,26 +8,31 @@ class PlayField(Canvas):
     def __init__(self, parent_frame):
         Canvas.__init__(self, parent_frame, width=self.width, height=self.height, bg="black")
         self.create_line(397, 0, 397, 500, fill="#ffffff", dash=(20, 10), width=6)
+        self.create_line(1, 1, 800, 1, fill="#ffffff", width=1)
+        self.create_line(800, 1, 800, 500, fill="#ffffff", width=1)
+        self.create_line(800, 500, 1, 500, fill="#ffffff", width=1)
+        self.create_line(1, 500, 1, 1, fill="#ffffff", width=1)
 
 
-class Paddle:
+class Paddle(Canvas):
     paddle_objects_created = 0
     x_padding = 10
     width = 12
     height = 80
-    speed = 2
+    speed = 10
 
     def __init__(self, play_field: Canvas):
+        super().__init__(play_field, width=self.width, height=self.height, bg="white")
         if Paddle.paddle_objects_created > 1:
             raise SystemError("Can't create more than 2 Paddle objects")
         Paddle.paddle_objects_created += 1
         x = self.x_padding if Paddle.paddle_objects_created == 1 else PlayField.width - self.x_padding - self.width
-        y = PlayField.height // 2 - self.height // 2
-        paddle = Canvas(play_field, width=self.width, height=self.height, bg="white")
-        paddle.place(x=x, y=y)
+        self.y_position = PlayField.height // 2 - self.height // 2
+        self.place(x=x, y=self.y_position)
 
     def move_up(self):
-        ...
+        self.y_position -= self.speed
+        self.place(y=self.y_position)
 
     def move_down(self):
         lowest_position = PlayField.height - self.height
--- Python/Pong/frames/game.py
+++ Python/Pong/frames/game.py
@@ -16,13 +16,20 @@ class Game(Frame):
 
         play_field = PlayField(self)
         play_field.grid(row=1)
-        paddle_1 = Paddle(play_field)
-        paddle_2 = Paddle(play_field)
+        self.paddle_1 = Paddle(play_field)
+        self.paddle_2 = Paddle(play_field)
+        self.test_label = ttk.Label(self)
+        self.test_label.grid(row=3)
+        self.controller.bind("<Key>", self.react)
 
     def ui(self):
         ui_frame = Frame(self, width=800, height=100)
-        button = ttk.Button(ui_frame, text="Main menu", style="W.TButton", command=lambda: self.controller.show_frame("MainMenu"))
+        button = ttk.Button(ui_frame, text="Main menu", style="W.TButton",
+                            command=lambda: self.controller.show_frame("MainMenu"))
         button.pack(anchor="e")
         score_board = ScoreBorad(ui_frame)
         score_board.pack(anchor="center")
         return ui_frame
+
+    def react(self, event):
+        self.paddle_1.move_up()
